d894e3079408d31e1aba27f978bde11788066cff
Roman <kydru4@gmail.com>
2022-06-29 20:41:32 +0300

    first commit

--- lib/Widgets/app.dart
+++ lib/Widgets/app.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_01/Widgets/car_lists.dart';
+import 'class_cars_Widgets/glc_class.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: CarListWidgets());
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => CarListWidgets(),
+        '/todo': (context) => const GlcClassWidget()
+      },
+    );
   }
 }
--- lib/Widgets/car_lists.dart
+++ lib/Widgets/car_lists.dart
@@ -12,13 +12,48 @@ class Car {
   });
 }
 
-class CarListWidgets extends StatelessWidget {
+class CarListWidgets extends StatefulWidget {
+  CarListWidgets({Key? key}) : super(key: key);
+
+  @override
+  State<CarListWidgets> createState() => _CarListWidgetsState();
+}
+
+class _CarListWidgetsState extends State<CarListWidgets> {
   final _cars = [
+    Car(
+      name: 'Maybach GLS',
+      sum: 'Від: 4 909 070.56 грн',
+      images: Image(
+        image: AssetImage('images/60706d4506b18.png'),
+      ),
+    ),
+    Car(
+      name: 'C-Клас Кабріо',
+      sum: 'Від: 1 322 238.83 грн',
+      images: Image(
+        image: AssetImage('images/606ed9343718d.png'),
+      ),
+    ),
     Car(
       name: 'GLC Купе',
       sum: 'Від: 1 470 524.27 грн',
       images: Image(
         image: AssetImage('images/606ed8c0840f7.png'),
+      ),
+    ),
+    Car(
+      name: 'S-Клас Седан',
+      sum: 'Від: 3 078 958.47 грн',
+      images: Image(
+        image: AssetImage('images/606ed7e35b1b4.png'),
+      ),
+    ),
+    Car(
+      name: 'Vito Tourer',
+      sum: 'Від: 1 114 211.12 грн',
+      images: Image(
+        image: AssetImage('images/60b64938de78f.png'),
       ),
     ),
     Car(
@@ -43,75 +78,122 @@ class CarListWidgets extends StatelessWidget {
       ),
     )
   ];
-  CarListWidgets({Key? key}) : super(key: key);
+  var _filtereCars = <Car>[];
+  final _searchController = TextEditingController();
+
+  void _searhCars() {
+    final qveri = _searchController.text;
+    if (qveri.isNotEmpty) {
+      _filtereCars = _cars.where(
+        (Car car) {
+          return car.name.toLowerCase().contains(qveri.toLowerCase());
+        },
+      ).toList();
+    } else {
+      _filtereCars = _cars;
+    }
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(_searhCars);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Модельний ряд'),
+        backgroundColor: Colors.black,
       ),
-      body: Container(
-        child: ListView.builder(
-          itemCount: _cars.length,
-          itemExtent: 250,
-          itemBuilder: (BuildContext context, int index) {
-            final car = _cars[index];
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-              child: Stack(
-                children: [
-                  Container(
-                    child: DecoratedBox(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.black.withOpacity(0.2),
-                        ),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(20),
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.1),
-                            blurRadius: 20,
-                          ),
-                        ],
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 15, vertical: 10),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    car.name,
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.w500,
-                                        fontSize: 24),
-                                  ),
-                                  Text(
-                                    car.sum,
-                                    style: TextStyle(color: Colors.grey),
-                                  )
-                                ],
-                              ),
+      body: Stack(
+        children: [
+          Container(
+            child: ListView.builder(
+              padding: EdgeInsets.only(top: 60),
+              itemCount: _filtereCars.length,
+              itemExtent: 300,
+              itemBuilder: (BuildContext context, int index) {
+                final car = _filtereCars[index];
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                  child: Stack(
+                    children: [
+                      Container(
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black.withOpacity(0.2),
                             ),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20),
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.1),
+                                blurRadius: 20,
+                              ),
+                            ],
                           ),
-                          car.images,
-                        ],
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 15, vertical: 15),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        car.name,
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 24),
+                                      ),
+                                      Text(
+                                        car.sum,
+                                        style: TextStyle(color: Colors.grey),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              car.images,
+                            ],
+                          ),
+                        ),
                       ),
-                    ),
+                      InkWell(
+                          borderRadius: BorderRadius.circular(20), onTap: () {})
+                    ],
                   ),
-                  InkWell(borderRadius: BorderRadius.circular(20), onTap: () {})
-                ],
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                label: Text('Пошук'),
+                prefixIcon: Icon(Icons.search),
+                filled: true,
+                fillColor: Colors.white.withAlpha(500),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                ),
               ),
-            );
-          },
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
